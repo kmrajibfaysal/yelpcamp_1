cedecf8b4f4ee5fc41f41e359dff1fa032c22ac0
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-09-17 12:09:28 +0600

    commiting to push on github

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const path = require('path');
+const Campground = require('./models/campground');
 
 const app = express();
-const path = require('path');
 
+// connection to database and error handler
+async function main() {
+    await mongoose.connect('mongodb://localhost:27017/yelp-camp');
+    console.log('Database Connected');
+}
+main().catch((err) => console.log(err));
+
+// render parameter
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
+// routes
 app.get('/', (req, res) => {
     res.render('home');
 });
+app.get('/makecampground', async (req, res) => {
+    const camp = new Campground({ title: 'My backyard', description: 'Cheap camping!' });
+    await camp.save();
+    res.send(camp);
+});
 
+// open port
 app.listen(3000, () => {
     console.log('Serving on port 3000!');
 });
