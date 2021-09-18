bd229ef897e4d0d8602bea0ed38e1754675fbc5e
K. M. Rajib Faysal <kmrajibfaysal@gmail.com>
2021-09-18 23:27:14 +0600

    adding style

--- seeds/index.js
+++ seeds/index.js
@@ -17,9 +17,14 @@ const seedDB = async () => {
     await Campground.deleteMany({});
     for (let i = 0; i < 50; i += 1) {
         const random1000 = Math.floor(Math.random() * 1000);
+        const price = Math.floor(Math.random() * 20) + 10;
         const camp = new Campground({
             location: `${cities[random1000].city}, ${cities[random1000].state}`,
             title: `${sample(descriptors)} ${sample(places)}`,
+            image: 'https://source.unsplash.com/collection/483251/900x600',
+            description:
+                'Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur exercitationem iusto soluta quisquam placeat dicta voluptatibus! Asperiores explicabo blanditiis et architecto, deserunt nisi saepe ducimus eos eius labore ipsa modi!',
+            price,
         });
         await camp.save();
     }
